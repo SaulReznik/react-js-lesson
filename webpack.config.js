825023a0546d4f2df0aa4162d6af0fc9a13fa1b7
noctifer <ohanjanyan.miqayel@gmail.com>
2017-05-30 18:48:17 +0400

    Init React Project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'es2016', 'es2017', 'stage-1']
+      }
+    }]
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    stats: 'errors-only'
+  }
+}
