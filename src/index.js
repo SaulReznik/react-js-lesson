b729abfc41d9a8beb09c858343cd1114bf9f7442
noctifer <ohanjanyan.miqayel@gmail.com>
2017-05-30 22:42:35 +0400

    add missing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      names:[
+        "1",
+        "2",
+        "3"
+      ]
+    };
+  }
+  inputChange = (e) => {
+    // console.log(e.target.value);
+    const val = e.target.value;
+    this.setState({
+      names: [
+        ...this.state.names,
+        val
+      ]
+    });
+  };
   render() {
-    return <h1>It Works</h1>;
+    return (
+      <div>
+        {this.state.names.map((elem, index) => {
+          return <h1 key={index}>{elem}</h1>
+        })}
+        <input type="text" onChange={this.inputChange} />
+    </div>);
   }
 }
 
