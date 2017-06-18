8c5d03bddc6395455115acd53db092ea91516ad6
roberthovhannsiyan <robhov97@gmail.com>
2017-06-18 17:11:47 +0400

    Add files via upload

---
+++ forum/app/components.js
@@ -0,0 +1,11 @@
+class Component extends React.Component {
+  render () {
+    return (
+      <div>Iam component</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Component/>,document.getElementById('app')
+)
